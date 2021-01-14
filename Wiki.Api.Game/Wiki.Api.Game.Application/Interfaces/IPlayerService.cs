338de2fe2988add64be7539e04502c2704fb7a16
NOURIACH <NOURIACH@LTWT618415>
2021-01-14 15:24:05 +0000

    Create PlayerService and Interface

---
+++ Wiki.Api.Game/Wiki.Api.Game.Application/Interfaces/IPlayerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Wiki.Api.Game.Domain.Models;
+
+namespace Wiki.Api.Game.Application.Interfaces
+{
+    interface IPlayerService
+    {
+        Task<Player> GetPlayerByIdAsync(int playerId);
+        Task<List<Player>> GetPlayersAsync();
+
+    }
+}
---
+++ Wiki.Api.Game/Wiki.Api.Game.Application/Services/PlayerService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Wiki.Api.Game.Application.Interfaces;
+using Wiki.Api.Game.Domain.Models;
+
+namespace Wiki.Api.Game.Application.Services
+{
+    public class PlayerService : IPlayerService
+    {
+        public Task<Player> GetPlayerByIdAsync(int playerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Player>> GetPlayersAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
