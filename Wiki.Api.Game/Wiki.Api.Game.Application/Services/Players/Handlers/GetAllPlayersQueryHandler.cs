4cc7c721aeea5cf2653e4b1c16b893d4261a5703
NOURIACH <NOURIACH@LTWT618415>
2021-01-14 15:50:08 +0000

    Build Mediatr Query Handlers skeleton

---
+++ Wiki.Api.Game/Wiki.Api.Game.Application/Services/Players/Handlers/GetAllPlayersQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Wiki.Api.Game.Application.Services.Players.Queries;
+using Wiki.Api.Game.Domain.Models;
+
+namespace Wiki.Api.Game.Application.Services.Players.Handlers
+{
+    public class GetAllPlayersQueryHandler : IRequestHandler<GetAllPlayersQuery, Player>
+    {
+        public Task<Player> Handle(GetAllPlayersQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Wiki.Api.Game/Wiki.Api.Game.Application/Services/Players/Handlers/GetPlayerQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Wiki.Api.Game.Application.Services.Players.Queries;
+using Wiki.Api.Game.Domain.Models;
+
+namespace Wiki.Api.Game.Application.Services.Players.Handlers
+{
+    public class GetPlayerQueryHandler : IRequestHandler<GetPlayerQuery, Player>
+    {
+        public Task<Player> Handle(GetPlayerQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
