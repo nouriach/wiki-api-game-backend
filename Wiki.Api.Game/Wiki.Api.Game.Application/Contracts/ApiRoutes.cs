eed199a79e69012e2b575436919d6ff9c9db09b5
NOURIACH <NOURIACH@LTWT618415>
2021-01-14 15:04:18 +0000

    Add new ApiRoutes class to Application.Contracts

---
+++ Wiki.Api.Game/Wiki.Api.Game.Application/Contracts/ApiRoutes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wiki.Api.Game.Application.Contracts
+{
+    public static class ApiRoutes
+    {
+        public const string Root = "api";
+        public const string Version = "v1";
+        public const string Base = Root + "/" + Version;
+
+        public static class Players
+        {
+            public const string GetAll = Base + "/players";
+            public const string Get = Base + "/players/{playerId}";
+            public const string Update = Base + "/players/{playerId}";
+            public const string Delete = Base + "/players/{playerId}";
+            public const string Create = Base + "/players";
+        }
+    }
+}
