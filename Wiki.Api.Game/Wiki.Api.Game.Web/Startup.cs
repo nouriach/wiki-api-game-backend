860bc9fc8cabf367ed8e8139cddc0f7e7cbd4fe6
NOURIACH <NOURIACH@LTWT618415>
2021-01-14 14:28:29 +0000

    Remove unnecessary code from Startup.cs

--- Wiki.Api.Game/Wiki.Api.Game.Web/Startup.cs
+++ Wiki.Api.Game/Wiki.Api.Game.Web/Startup.cs
@@ -42,20 +42,15 @@ namespace Wiki.Api.Game.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
-
-            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
