3ccc7556065329c77cc2276e101c684d4f540f01
NOURIACH <NOURIACH@LTWT618415>
2021-01-14 17:05:48 +0000

    Setup AppDbContext in the Persistence project

---
+++ Wiki.Api.Game/Wiki.App.Game.Persistence/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Wiki.Api.Game.Domain.Models;
+
+namespace Wiki.Api.Game.Persistence.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Player> Players { get; set; }
+
+    }
+}
