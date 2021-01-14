d11c1ebeeae4e82cab3300e28dbfe58e0849f172
NOURIACH <NOURIACH@LTWT618415>
2021-01-14 17:07:22 +0000

    Migrate & Update DB

---
+++ Wiki.Api.Game/Wiki.App.Game.Persistence/Migrations/20210114164557_InitialMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Wiki.Api.Game.Persistence.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nationality = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Position = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstPremierLeagueAppearance = table.Column<int>(type: "int", nullable: false),
+                    LastPremierLeagueAppearance = table.Column<int>(type: "int", nullable: false),
+                    TotalAppearances = table.Column<int>(type: "int", nullable: false),
+                    Clubs = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
