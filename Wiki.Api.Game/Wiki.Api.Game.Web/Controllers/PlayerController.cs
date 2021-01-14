39e832ae3d5d887079ef31bc71e994fda78503ae
NOURIACH <NOURIACH@LTWT618415>
2021-01-14 14:44:17 +0000

    Setup simple Controller endpoints

---
+++ Wiki.Api.Game/Wiki.Api.Game.Web/Controllers/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wiki.Api.Game.Web.Controllers
+{
+    public class PlayerController : Controller
+    {
+        [HttpGet("api/v1/players/{playerId}")]
+
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("api/v1/players")]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+    }
+}
