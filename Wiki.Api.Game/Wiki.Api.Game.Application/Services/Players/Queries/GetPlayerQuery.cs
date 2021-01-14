8123e3f7311b35c1508420452a219094952f0e52
NOURIACH <NOURIACH@LTWT618415>
2021-01-14 15:46:29 +0000

    Setup Query classes

---
+++ Wiki.Api.Game/Wiki.Api.Game.Application/Services/Players/Queries/GetPlayerQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Wiki.Api.Game.Domain.Models;
+
+namespace Wiki.Api.Game.Application.Services.Players.Queries
+{
+    public class GetPlayerQuery : IRequest<Player>
+    {
+    }
+}
