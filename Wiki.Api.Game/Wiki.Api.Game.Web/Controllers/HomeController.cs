255b427ffd62b9f3277e969472e2a04e07ffc744
NOURIACH <NOURIACH@LTWT618415>
2021-01-15 16:57:02 +0000

    Update HomeController frontend to display JSON example and early documentation structure

--- Wiki.Api.Game/Wiki.Api.Game.Web/Controllers/HomeController.cs
+++ Wiki.Api.Game/Wiki.Api.Game.Web/Controllers/HomeController.cs
@@ -1,24 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Wiki.Api.Game.Domain.Models;
+using Wiki.Api.Game.Persistence.Data;
 
 namespace Wiki.Api.Game.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            List<Player> players = _context.Players.Take(3).ToList();
+
+            string json = JsonConvert.SerializeObject(players, Formatting.Indented);
+            json = json.Replace("[", "");
+            json = json.Replace("]", "");
+            ViewBag.Json = json;
             return View();
         }
 
