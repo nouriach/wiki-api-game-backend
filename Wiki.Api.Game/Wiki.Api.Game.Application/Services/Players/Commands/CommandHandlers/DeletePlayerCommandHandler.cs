d1b8d54093a14a423497b5dace2addc53c8df104
NOURIACH <NOURIACH@LTWT618415>
2021-01-15 13:30:11 +0000

    Add Mediatr Command & Handler for Delete request

---
+++ Wiki.Api.Game/Wiki.Api.Game.Application/Services/Players/Commands/CommandHandlers/DeletePlayerCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Wiki.Api.Game.Application.Interfaces;
+using Wiki.Api.Game.Application.Services.Players.Commands.CommandQueries;
+
+namespace Wiki.Api.Game.Application.Services.Players.Commands.CommandHandlers
+{
+    public class DeletePlayerCommandHandler : IRequestHandler<DeletePlayerCommand, bool>
+    {
+        private readonly IPlayerService _playerService;
+
+        public DeletePlayerCommandHandler(IPlayerService playerService)
+        {
+            _playerService = playerService;
+        }
+
+        public async Task<bool> Handle(DeletePlayerCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _playerService.DeletePlayerAsync(request.Id);
+            return result;
+        }
+    }
+}
---
+++ Wiki.Api.Game/Wiki.Api.Game.Application/Services/Players/Commands/CommandQueries/DeletePlayerCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wiki.Api.Game.Application.Services.Players.Commands.CommandQueries
+{
+    public class DeletePlayerCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+
+    }
+}
